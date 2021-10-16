875fc2316e9af9107f28ff4286c819c13713486e
PauloJsReact <paulo_henrique_araujo@outlook.com>
2021-10-16 19:23:48 -0300

    New parameters were added, to reverse the position of a cell in a stack, the one that is printing on the terminal.

---
+++ ex004/index.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct node
+{
+    int info;
+    struct node * next;  
+};
+
+
+struct stack
+{
+    struct node * top;
+    int size ;
+};
+
+struct stack*create(){
+    struct stack * new_stack = (struct stack*) malloc(sizeof(struct stack));
+
+    if (new_stack !=NULL)
+    {
+        new_stack ->top = NULL;
+        new_stack->size = 0;
+    }
+    return new_stack;
+}
+
+void stackUp(struct stack* s,int item){
+    assert(s != NULL);
+    struct node * new_node = (struct node *)malloc(sizeof(struct node));
+
+    if (new_node != NULL)
+    {
+        new_node->info=item;
+        new_node->next=s->top;
+        s->top=new_node;
+        s->size++;
+    }
+}
+
+int unStack(struct stack* s){
+    assert(s !=  NULL);
+    assert(s->top != NULL);
+    struct node * aux = s->top;
+    int element =  aux->info;
+    s->top = aux->next;
+    s->size -- ;
+    free(aux);
+    return element;
+}
+
+int main(){
+    struct stack * my_stack = create();
+     int num;
+     for (int i = 0; i < 5; i++)
+     {
+        printf("\n  Insert one int number :");
+        scanf("%d",&num);
+        stackUp(my_stack,num);
+     }
+
+     for (int i = 0; i < 5; i++)
+     {
+       printf("%d ",unStack(my_stack));
+     }  
+    return 0 ;
+}
